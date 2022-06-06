e6a1efb3147e06a9f1f8ac4e8b290f7896ac6c67
andreioprea99 <anddor99@gmail.com>
2022-06-06 14:53:04 +0300

    Add project files.

---
+++ Controllers/GenerateCodeController.cs
@@ -0,0 +1,29 @@
+﻿using CodeGenerator.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeGenerator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenerateCodeController : ControllerBase
+    {
+        private readonly ILogger<MainRequest> _logger;
+
+        public GenerateCodeController(ILogger<MainRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MainRequest>> Post(MainRequest request)
+        {
+            return request;
+        }
+    }
+}
